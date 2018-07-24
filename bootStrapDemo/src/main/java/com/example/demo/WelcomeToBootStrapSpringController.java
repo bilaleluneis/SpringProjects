9634746a0a9ae69f53f2f3288ad7be63cd2fe9cd
bilaleluneis <bilaleluneis@gmail.com>
2018-07-24 00:00:59 -0400

    simple initial controller

---
+++ bootStrapDemo/src/main/java/com/example/demo/WelcomeToBootStrapSpringController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Bilal El Uneis
+ * @since July 2018
+ * Sample controller for spring boot with thymeleaf and Bootstrap
+ */
+
+@Controller
+public class WelcomeToBootStrapSpringController {
+	
+	/**
+	 * gets called when hitting localhost:8080/ and getting that page
+	 * @param model
+	 * @return src/main/resources/templates/views/welcome.html page
+	 */
+	@RequestMapping( value="/" , method = RequestMethod.GET )
+	public String index ( Model model ) {
+		return "views/welcome";
+	}
+
+}
