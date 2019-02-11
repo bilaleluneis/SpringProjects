602fd92be8ba6ffee9cbe90b692775f50f4947b7
bilaleluneis <bilaleluneis@gmail.com>
2019-02-11 16:53:32 -0700

    added author

--- springConstrainValidation/src/main/java/com/example/demo/SpringConstrainValidationApplication.java
+++ springConstrainValidation/src/main/java/com/example/demo/SpringConstrainValidationApplication.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Bilal El Uneis (bilaleluneis@gmail.com)
+ * @since Feb 2019
+ */
 @SpringBootApplication
 public class SpringConstrainValidationApplication {
 
